b69dc31a8c58eebf7ebcae27d97e9a771308cf3b
Prakhar <Prakhar@DESKTOP-UDKS47A>
2018-03-30 23:07:43 +0530

    Importing Project

---
+++ Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B obj = new B();
+            Console.ReadKey(); 
+        }
+    }
+
+
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("This is A");
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("This is B");
+        }
+    }
+
+
+    
+
+    interface TestInterface
+    {
+
+    }
+
+
+
+
+}
---
+++ Test/StaticConstructor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class StaticConstructor
+    {
+        public StaticConstructor()
+        {
+            Console.WriteLine("This is PublicConstructor");
+        }
+        static StaticConstructor()
+        {
+            Console.WriteLine("This is StaticConstructor");
+        }
+    }
+
+    public class StaticConstructorOne : StaticConstructor
+    {
+        public StaticConstructorOne()
+        {
+            Console.WriteLine("This is PublicConstructorOne");
+        }
+        static StaticConstructorOne()
+        {
+            Console.WriteLine("This is StaticConstructorOne");
+        }
+
+
+    }
+
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var sc = new StaticConstructorOne();
+            Console.ReadKey();
+        }
+    }
+
+
+    //A static constructor is used to initialize any static data, or to perform a particular action that needs to be performed once only.
+    //    It is called automatically before the first instance is created or any static members are referenced.
+
+
+}
